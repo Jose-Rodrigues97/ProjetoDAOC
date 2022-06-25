915051e4ad426104bb8e748a6a3c9229e2d208b2
José Evandro Rodrigues Filho <zedepoa@gmail.com>
2022-06-25 10:57:26 -0300

    Criando a pasta Componentes

--- Projeto/src/app/app-routing.module.ts
+++ Projeto/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CadastrodoadorComponentComponent } from './cadastrodoador-component/cadastrodoador-component.component';
-import { CadastroestabelecimentoComponentComponent } from './cadastroestabelecimento-component/cadastroestabelecimento-component.component';
-import { CadastronecessitadoComponentComponent } from './cadastronecessitado-component/cadastronecessitado-component.component';
-import { CardsComponent } from './cards-component/cards.component';
-import { ContatosComponent } from './contatos-component/contatos-component.component';
-import { PorquedoarComponent } from './porquedoar-component/porquedoar.component';
-import { QuemsomosComponent } from './quemsomos-component/quemsomos.component';
+import { CadastrodoadorComponentComponent } from './Componentes/cadastrodoador-component/cadastrodoador-component.component';
+import { CadastroestabelecimentoComponentComponent } from './Componentes/cadastroestabelecimento-component/cadastroestabelecimento-component.component';
+import { CadastronecessitadoComponentComponent } from './Componentes/cadastronecessitado-component/cadastronecessitado-component.component';
+import { CardsComponent } from './Componentes/cards-component/cards.component';
+import { ContatosComponent } from './Componentes/contatos-component/contatos-component.component';
+import { LoginComponentComponent } from './Componentes/login-component/login-component.component';
+import { PorquedoarComponent } from './Componentes/porquedoar-component/porquedoar.component';
+import { QuemsomosComponent } from './Componentes/quemsomos-component/quemsomos.component';
 
 const routes: Routes = [
+  {
+    path: 'Login',
+    component:LoginComponentComponent
+  },
   {
     path: 'QuemSomos',
     component: QuemsomosComponent
--- Projeto/src/app/app.module.ts
+++ Projeto/src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardsComponent } from './cards-component/cards.component';
-import { CarouselComponentComponent } from './carousel-component/carousel-component.component';
-import { HeaderComponentComponent } from './header-component/header-component.component';
-import { FooterComponentComponent } from './footer-component/footer-component.component';
-import { QuemsomosComponent } from './quemsomos-component/quemsomos.component';
-import { PorquedoarComponent } from './porquedoar-component/porquedoar.component';
-import { ContatosComponent } from './contatos-component/contatos-component.component';
-import { CadastrodoadorComponentComponent } from './cadastrodoador-component/cadastrodoador-component.component';
-import { CadastronecessitadoComponentComponent } from './cadastronecessitado-component/cadastronecessitado-component.component';
-import { CadastroestabelecimentoComponentComponent } from './cadastroestabelecimento-component/cadastroestabelecimento-component.component';
-import { LoginComponentComponent } from './login-component/login-component.component';
+import { CardsComponent } from './Componentes/cards-component/cards.component';
+import { CarouselComponentComponent } from './Componentes/carousel-component/carousel-component.component';
+import { HeaderComponentComponent } from './Componentes/header-component/header-component.component';
+import { FooterComponentComponent } from './Componentes/footer-component/footer-component.component';
+import { QuemsomosComponent } from './Componentes/quemsomos-component/quemsomos.component';
+import { PorquedoarComponent } from './Componentes/porquedoar-component/porquedoar.component';
+import { ContatosComponent } from './Componentes/contatos-component/contatos-component.component';
+import { CadastrodoadorComponentComponent } from './Componentes/cadastrodoador-component/cadastrodoador-component.component';
+import { CadastronecessitadoComponentComponent } from './Componentes/cadastronecessitado-component/cadastronecessitado-component.component';
+import { CadastroestabelecimentoComponentComponent } from './Componentes/cadastroestabelecimento-component/cadastroestabelecimento-component.component';
+import { LoginComponentComponent } from './Componentes/login-component/login-component.component';
 
 @NgModule({
   declarations: [
